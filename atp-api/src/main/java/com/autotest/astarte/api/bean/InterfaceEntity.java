615c245cd3ef97f3acfd5b024c1f0bc2633188ed
86182 <18271671995@163.com>
2022-11-17 17:28:06 +0800

    by lsw

---
+++ atp-api/src/main/java/com/autotest/astarte/api/bean/InterfaceEntity.java
@@ -0,0 +1,11 @@
+package com.autotest.astarte.api.bean;
+
+import lombok.Data;
+
+@Data
+public class InterfaceEntity {
+    Long id;
+    String interface_name;
+    String interface_path;
+    String interface_type;
+}
